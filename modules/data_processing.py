6e852e36114db82c52177353c366a3decca6696e
Sam_dev <102005672+Samilincoln@users.noreply.github.com>
2026-07-15 00:53:08 +0100

    fix: deprecated imports

--- modules/data_processing.py
+++ modules/data_processing.py
@@ -1,6 +1,6 @@
 #=======================Convert df into document==================
 
-from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
