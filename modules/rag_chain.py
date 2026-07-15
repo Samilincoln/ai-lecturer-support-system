9f2500387b5d20660e7787b1cd737e26009e70b4
Sam_dev <102005672+Samilincoln@users.noreply.github.com>
2026-07-15 01:04:35 +0100

    Update rag_chain.py

--- modules/rag_chain.py
+++ modules/rag_chain.py
@@ -3,7 +3,7 @@
 
 
 from langchain_classic.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 
